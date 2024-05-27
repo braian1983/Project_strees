ea4bf6da495eedbda01c5cc99691b3eee5bf4c5c
Braian Cruz <87029054+braian1983@users.noreply.github.com>
2024-05-27 09:01:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,24 +11,23 @@ def check_stress_level(stress_level):
         return 5    
     else:
         return 0    
-def add_user(user_id: int) -> None:
+  def add_user(user_id: int) -> None:
     
-    if user_id not in stress_points:
-        stress_points[user_id] = 0
+         if user_id not in stress_points:
+         stress_points[user_id] = 0
 
-def update_stress_score(user_id: int, stress_level: int) -> None:
+      def update_stress_score(user_id: int, stress_level: int) -> None:
     
-    points = check_stress_level(stress_level)
-    if user_id in stress_points:
-        stress_points[user_id] += points
-    else:
-        print(f"Usuário {user_id} não encontrado. Adicionando ao sistema.")
-        add_user(user_id)
-        stress_points[user_id] += points
+           points = check_stress_level(stress_level)
+           if user_id in stress_points:
+           stress_points[user_id] += points
+           else:
+            print(f"Usuário {user_id} não encontrado. Adicionando ao sistema.")
+            add_user(user_id)
+             stress_points[user_id] += points
 
-def get_stress_score(user_id: int) -> int:
-     
-    return stress_points.get(user_id, 0)
+     def get_stress_score(user_id: int) -> int:
+         return stress_points.get(user_id, 0)
 
 def log_stress_level(user_id: int, stress_level: int) -> None:
      
@@ -37,12 +36,12 @@ def log_stress_level(user_id: int, stress_level: int) -> None:
     print(f"{timestamp} - Usuário {user_id} registrou um nível de estresse de {stress_level} e ganhou {points} pontos.")
     update_stress_score(user_id, stress_level)
 
-def Hours(range)
+  def Hours(range)
     hours = range(1, 13) 
      points = [5* h for in hours]
-for h, p in zip (hours, points):
-    print('hour %2i: %2i points' % (h, p))
-    hours = range(6, 21)
-     points = [10 * h for h in hours]
-for h, p in zip(hours, points):
-    print('Hour %2i: %2i points' % (h, p))
+    for h, p in zip (hours, points):
+      print('hour %2i: %2i points' % (h, p))
+       hours = range(6, 21)
+        points = [10 * h for h in hours]
+      for h, p in zip(hours, points):
+          print('Hour %2i: %2i points' % (h, p))
