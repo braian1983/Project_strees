be4788f075ad844d8c18f4689954cbf3f43d1fbf
Braian Cruz <87029054+braian1983@users.noreply.github.com>
2024-05-28 08:45:33 -0300

    Update hours.py

--- hours.py
+++ hours.py
@@ -2,12 +2,12 @@ import time
 
 hours_points = []
 
- def Hours(range)
-    hours = range(1, 13) 
-     points = [5* h for in hours]
-   for h, p in zip (hours, points):
-      print('hour %2i: %2i points' % (h, p))
-       hours = range(6, 21)
-        points = [10 * h for h in hours]
-    for h, p in zip(hours, points):
-          print('Hour %2i: %2i points' % (h, p))
+def Hours(range)
+hours = range(1, 13) 
+points = [5* h for in hours]
+for h, p in zip (hours, points):
+ print('hour %2i: %2i points' % (h, p))
+ hours = range(6, 21)
+ points = [10 * h for h in hours]
+ for h, p in zip(hours, points):
+  print('Hour %2i: %2i points' % (h, p))
