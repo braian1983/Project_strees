5398410f3031cea31e1f94e5bb1bf6d8c8da1810
Braian Cruz <87029054+braian1983@users.noreply.github.com>
2024-06-04 06:59:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,33 @@
 import datetime
-import SpeechRecognition as sr # type: ignore
 import time
+import SpeechRecognition as sr # type: ignore
 
 stress_points = {}
 
 def check_stress_level(stress_level):
-    if stress_level < 5:
-        return 10  
-    elif stress_level < 8:
-        return 5
-    else:
-        return 0   
-
+   if stress_level < 5:
+      return 10 
+   elif stress_level < 8:
+     return 5    
+   else:
+       return 0   
 def add_user(user_id: int) -> None:
     
  if user_id not in stress_points:
      stress_points[user_id] = 0
 
 def update_stress_score(user_id: int, stress_level: int) -> None:
-    
-points = check_stress_level(stress_level)
+    points = check_stress_level(stress_level)
     if user_id in stress_points:
-     stress_points[user_id] += points
+          stress_points[user_id] += points
     else:
-     print(f"Usuário {user_id} não encontrado. Adicionando ao sistema.")
-     add_user(user_id)
-     stress_points[user_id] += points
+          print(f"Usuário {user_id} não encontrado. Adicionando ao sistema.")
+          add_user(user_id)
+          stress_points[user_id] += points
 
 def get_stress_score(user_id: int) -> int:
-return stress_points.get(user_id, 0)
-
-def accumulate(interable, function=operator.add, *, initial=none):
-    'return running totals'
-
-    interator = iter(iterable)
-    total = initial 
-    initial is none:
-      try:
-          total = next(iterator)
-     except StopIterator:
-         return
-  yield total
- for element in iterator:
-     total = function(total, element)
-     yield total
+     
+     return stress_points.get(user_id, 0)
 
 def log_stress_level(user_id: int, stress_level: int) -> None:
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -56,12 +40,11 @@ def listen_voice():
     with sr.Microphone() as source:
         print("Diga algo...")
         audio = r.listen(source)
-        
         try:
             text = r.recognize_google(audio, language='pt-BR')
             print("Você disse: " + text)
             return text
-            except:
+        except:
                 print("Não foi possível reconhecer a fala.")
                 return None
 
